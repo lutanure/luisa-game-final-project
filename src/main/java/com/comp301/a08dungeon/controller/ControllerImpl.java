60fd26677f1e04df15f47cfe199a4001106c144e
Luisa Tanure <lutanure@unc.edu>
2025-04-25 17:37:57 -0400

    novice - t1 - controllerImpl

--- src/main/java/com/comp301/a08dungeon/controller/ControllerImpl.java
+++ src/main/java/com/comp301/a08dungeon/controller/ControllerImpl.java
@@ -1,5 +1,28 @@
 package com.comp301.a08dungeon.controller;
 
-public class ControllerImpl {
+import com.comp301.a08dungeon.model.Model;
 
+public class ControllerImpl implements Controller {
+  private final Model model;
+
+  public ControllerImpl(Model model) {
+    this.model = model;
+  } 
+
+  public void moveUp(){
+    model.moveUp();
+  }
+  public void moveDown(){
+    model.moveDown();
+  }
+  public void moveRight(){
+    model.moveRight();
+  }
+  public void moveLeft(){
+    model.moveLeft();
+  }
+
+  public void startGame(){
+    model.startGame();
+  }
 }
