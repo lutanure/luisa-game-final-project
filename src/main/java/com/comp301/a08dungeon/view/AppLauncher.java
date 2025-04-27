d476eb35a024d56a9c1de5b23a06b333217092cd
Luisa Tanure <lutanure@unc.edu>
2025-04-27 19:03:17 -0400

    adept - t1

--- src/main/java/com/comp301/a08dungeon/view/AppLauncher.java
+++ src/main/java/com/comp301/a08dungeon/view/AppLauncher.java
@@ -1,11 +1,27 @@
 package com.comp301.a08dungeon.view;
 
+import com.comp301.a08dungeon.controller.ControllerImpl;
+import com.comp301.a08dungeon.model.Model;
+import com.comp301.a08dungeon.model.ModelImpl;
+
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class AppLauncher extends Application {
   @Override
   public void start(Stage stage) {
-      stage.show();
+    stage.setTitle("lu's Dungeon Crawler");
+    
+    Model model = new ModelImpl(10, 10);
+    ControllerImpl controller = new ControllerImpl(model);
+    View view = new View(controller, model, stage, 600, 600);
+    Scene scene = new Scene(view.render(), 600, 600);
+
+    scene.getStylesheets().add("dungeon.css");
+    
+    stage.setScene(scene);
+
+    stage.show();
   }
 }
--- src/main/java/com/comp301/a08dungeon/view/View.java
+++ src/main/java/com/comp301/a08dungeon/view/View.java
@@ -1,15 +1,28 @@
 package com.comp301.a08dungeon.view;
 
+import com.comp301.a08dungeon.controller.Controller;
+import com.comp301.a08dungeon.model.Model;
 import com.comp301.a08dungeon.model.Observer;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
 public class View implements FXComponent, Observer {
-
-  public View(){
-
+  private final Controller controller;
+  private final Model model;
+  private final Stage stage;
+  private final int windowWidth;
+  private final int windowHeight;
+  
+  public View(Controller controller, Model model, Stage stage, int windowWidth, int windowHeight) {
+    this.controller = controller;
+    this.model = model;
+    this.stage = stage;
+    this.windowWidth = windowWidth;
+    this.windowHeight = windowHeight;
   }
 
 
@@ -21,6 +34,8 @@ public class View implements FXComponent, Observer {
 
   @Override
   public void update() {
-
+    Scene scene = new Scene(render(), windowWidth, windowHeight);
+    scene.getStylesheets().add("dungeon.css");
+    stage.setScene(scene);
   }
 }
